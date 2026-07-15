b8318f1f78d2db3cc52a2539a9cceb8f358ca88f
Keshav Gaur <keshav18gaur@gmail.com>
2026-07-15 23:06:22 +0530

    Add Remove Dup From Sorted Array

---
+++ arrays/Remove_dup_from_sortted_array.cpp
@@ -0,0 +1,23 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    int j=0;
+    for(int i=0;i<n;i++){
+        if(nums[i]!=nums[j]){
+            j++;
+            nums[j]=nums[i];
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<nums[i]<<" ";
+    }
+    return 0;
+}
